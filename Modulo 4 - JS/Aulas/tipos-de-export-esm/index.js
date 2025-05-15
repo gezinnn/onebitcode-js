334ec97d3448add0bb69eda013673fbfab738894
Geovannni Almeida <131813766+gezinnn@users.noreply.github.com>
2025-05-15 16:29:23 -0300

    aula sobre exports ESM

---
+++ Modulo 4 - JS/Aulas/tipos-de-export-esm/index.js
@@ -0,0 +1,10 @@
+import { inLine } from "./inline.js"
+import defaultInLine from "./inline.js"
+import { group } from "./non-inline.js"
+import exportDefault from "./non-inline.js"
+
+inLine()
+defaultInLine()
+
+group()
+exportDefault()
