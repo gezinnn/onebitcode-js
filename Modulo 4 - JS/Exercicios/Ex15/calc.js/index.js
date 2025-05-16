6471234640ee4e68e626334158f6959ecb106136
Geovannni Almeida <131813766+gezinnn@users.noreply.github.com>
2025-05-15 21:02:26 -0300

    usando os modulos no projeto calc.js

---
+++ Modulo 4 - JS/Exercicios/Ex15/calc.js/index.js
@@ -0,0 +1,62 @@
+import { input } from "./dom.js";
+import calculate from "./calculate.js";
+import themeSwitcher from "./switchertheme.js";
+import copyToClipboard from "./copytoclipboard.js";
+
+const allowedKeys = [
+  "(",
+  ")",
+  "/",
+  "*",
+  "-",
+  "+",
+  "9",
+  "8",
+  "7",
+  "6",
+  "5",
+  "4",
+  "3",
+  "2",
+  "1",
+  "0",
+  ".",
+  "%",
+  " ",
+];
+
+document.querySelectorAll(".charKey").forEach(function (charKeyBtn) {
+  charKeyBtn.addEventListener("click", function () {
+    const value = charKeyBtn.dataset.value;
+    input.value += value;
+  });
+});
+
+document.getElementById("clear").addEventListener("click", function () {
+  input.value = "";
+  input.focus();
+});
+
+document.getElementById("equal").addEventListener("click", calculate);
+
+themeSwitcher()
+
+copyToClipboard()
+
+input.addEventListener("keydown", function (ev) {
+  ev.preventDefault();
+  if (allowedKeys.includes(ev.key)) {
+    input.value += ev.key;
+    return;
+  }
+  if (ev.key === "Backspace") {
+    input.value = input.value.slice(0, -1);
+  }
+
+  if (ev.key === "Enter") {
+    calculate();
+  }
+});
+
+calculate()
+
