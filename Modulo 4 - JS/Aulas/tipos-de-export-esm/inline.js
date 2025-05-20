091cd8509f67ea6382259dd99bdd3ba98cfa476d
Geovannni Almeida <131813766+gezinnn@users.noreply.github.com>
2025-05-20 19:51:34 -0300

    reestruturando as pastas e arquivos

--- Modulo 4 - JS/Aulas/tipos-de-export-esm/inline.js
+++
@@ -1,7 +0,0 @@
-export function inLine(){
-  console.log("Export nomeado inline")
-}
-
-export default function defaultInLine(){
-  console.log("Export default inline")
-}
--- Modulo 4 - JS/Exercicios/Ex15/calc.js/dom.js
+++
@@ -1,6 +0,0 @@
-export const main = document.querySelector("main");
-export const root = document.querySelector(":root");
-export const input = document.getElementById("input");
-export const resultInput = document.getElementById("result");
-
-
